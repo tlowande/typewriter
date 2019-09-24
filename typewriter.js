a4814ea12d5325f90976d3e83fc1defd2be3ae05
Tamires <tamires.lowande@gmail.com>
2019-09-24 20:47:29 +0000

    timer set!

--- typewriter.js
+++ typewriter.js
@@ -4,6 +4,7 @@ for (let char of sentence){
   setTimeout(() => {
     process.stdout.write(char)
 }, time)
+  console.log(time);
   time += 50;
   if(time === 50 * sentence.length){
     char += '\n';
